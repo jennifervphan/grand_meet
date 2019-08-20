5d25f4a65267121b8f4f69163d423450359cd874
Jennifer V Phan <jennyphan181295@gmail.com>
2019-08-20 21:10:12 +0200

    test test

--- grandMeet-server/app.js
+++ grandMeet-server/app.js
@@ -106,9 +106,9 @@ app.use('/api', require('./routes/newChat'));
 app.use('/api', require('./routes/share'));
 app.use('/api', require('./routes/game'));
 
-// app.use((req, res, next) => {
-//     // If no routes match, send them the React HTML.
-//     res.sendFile(__dirname + "/public/index.html");
-// });
+app.use((req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 module.exports = app;
